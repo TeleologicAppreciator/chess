6ce7462f2fccaecd207cd3c5dbe32df9655e3980
Teleologic-Appreciator <brycejconrad@gmail.com>
2024-07-22 00:05:40 -0600

    General changes to better fit coding guidelines.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,26 +1,49 @@
 package server;
 
-import org.eclipse.jetty.client.HttpResponseException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import model.AuthData;
+import model.UserData;
 import service.DeleteService;
 import spark.*;
 
-public class Server {
-    private final DeleteService myService;
+import java.util.UUID;
 
-    public Server(DeleteService theService) {
-        myService = theService;
+public class Server {
+    private final DeleteService myDeleteService;
+    private final UserService myUserService;
+
+    public Server() {
+        var userData = new UserData("king", "theonly", "kingkingcastle@gmail.com");
+        var userDataStorage = new MemoryUserDAO();
+        userDataStorage.createUser(userData);
+
+        var uniqueID = new UUID(100, 150);
+        String kingAuthToken = UUID.randomUUID().toString();
+        var authData = new AuthData(kingAuthToken, userData.myUsername());
+        var authDataStorage = new MemoryAuthDAO();
+        authDataStorage.createAuth(authData);
+
+        var gameDataStorage = new MemoryGameDAO();
+        gameDataStorage.createGame("kings game");
+
+        var service = new DeleteService(userDataStorage, authDataStorage, gameDataStorage);
+
+        myDeleteService = service;
     }
 
-    public Server run(int desiredPort) {
+    public int run(int desiredPort) {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", this::createUser);
         Spark.delete("/db", this::deleteAll);
 
         Spark.awaitInitialization();
-        return this;
+        return Spark.port();
     }
 
     public void stop() {
@@ -28,10 +51,14 @@ public class Server {
         Spark.awaitStop();
     }
 
-    private Object deleteAll(Request req, Response res) {
-        myService.deleteAllData();
+    private Object createUser(Request theRequest, Response theResponse) {
+        myService.createUser();
+    }
 
-        res.status(200);
+    private Object deleteAll(Request theRequest, Response theResponse) {
+        myDeleteService.deleteAllData();
+
+        theResponse.status(200);
         return "";
     }
 }
