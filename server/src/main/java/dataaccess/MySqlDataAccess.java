24f99c30b9eea03f6a01650ce574d70da54b15dd
Teleologic-Appreciator <brycejconrad@gmail.com>
2024-07-27 23:50:27 -0600

    Implemented the ability to create a new database.

---
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -0,0 +1,46 @@
+package dataaccess;
+
+import java.sql.SQLException;
+
+public class MySqlDataAccess {
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  game (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `whiteUsername` varchar(256) DEFAULT NULL,
+              `blackUsername` varchar(256) DEFAULT NULL,
+              `name` varchar(256) NOT NULL,
+              `json` TEXT NOT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(whiteUsername),
+              INDEX(blackUsername),
+              INDEX(name)
+            ) CREATE TABLE IF NOT EXISTS  user (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              PRIMARY KEY (`username`),
+              INDEX(password)
+            ) CREATE TABLE IF NOT EXISTS  auth (
+              `authToken` varchar(256) DEFAULT NULL,
+              `username` varchar(256) DEFAULT NULL,
+              PRIMARY KEY (`authToken`),
+              INDEX(username)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", e.getMessage()));
+        }
+    }
+}
