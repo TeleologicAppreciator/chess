95209e42e2c38f26f8fc7bad50d11d221091c727
Teleologic-Appreciator <brycejconrad@gmail.com>
2024-07-30 20:28:10 -0600

    Implemented MySqlGameDAO getGame.

--- server/src/main/java/dataaccess/MySqlGameDAO.java
+++ server/src/main/java/dataaccess/MySqlGameDAO.java
@@ -1,7 +1,9 @@
 package dataaccess;
 
 import chess.ChessGame;
+import model.AuthData;
 import model.GameData;
+import serialization.Deserializer;
 import serialization.Serializer;
 
 import java.sql.DriverManager;
@@ -42,7 +44,34 @@ public class MySqlGameDAO extends MySqlDataAccess implements GameDAO {
     }
 
     public GameData getGame(int theGameID) throws DataAccessException {
-        return null;
+        try (var connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/mydb", "root", "Mypasswordformysqlserver50!")) {
+            var statement = "SELECT id FROM game WHERE id = ?";
+
+            try (var preparedStatement = connection.prepareStatement(statement)) {
+                preparedStatement.setString(1, Integer.toString(theGameID));
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                String gameIDIsTheSame = null;
+                while (resultSet.next()) {
+                    gameIDIsTheSame = resultSet.getString("authToken");
+                    if (gameIDIsTheSame.equals(Integer.toString(theGameID))) {
+                        String white = resultSet.getString("whiteUsername");
+                        String black = resultSet.getString("blackUsername");
+                        String name = resultSet.getString("name");
+                        ChessGame game = (ChessGame)
+                                new Deserializer(resultSet.getString("json"), ChessGame.class).deserialize();
+
+                        return new GameData(theGameID, white, black, name, game);
+                    }
+                }
+
+                throw new DataAccessException("Game not found");
+            }
+        } catch (Exception e) {
+            throw new DataAccessException("Unable to read data");
+        }
     }
 
     public GameData[] getAllGames() {
