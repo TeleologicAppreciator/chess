5d7a4f1100edd3682120f07336fb4ce4c4b97472
Teleologic-Appreciator <brycejconrad@gmail.com>
2024-08-03 13:57:50 -0600

    ChessClient list all games implemented and creating a game.

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -12,6 +12,7 @@ public class ChessClient {
     private String visitorName = null;
     private State state = State.SIGNEDOUT;
     private AuthData authData = null;
+    private GameData[] gameData = null;
 
     public ChessClient(String theServerUrl) {
         server = new ServerFacade(theServerUrl);
@@ -34,14 +35,34 @@ public class ChessClient {
             return String.format("Welcome %s", userToLogin.username());
     }
 
+    public String create(String theGameName) throws DataAccessException {
+        //the server does not grab the gameID int value
+        server.createGame(new GameData(Integer.MIN_VALUE, null, null, theGameName, null), authData);
+
+        return "Successfully created game: " + theGameName;
+    }
+
+    public String joinGame(int gameID, String playerColor) throws DataAccessException {
+        server.joinGame();
+    }
+
     public String list() throws DataAccessException {
         GameData[] listOfAllGames = server.getAllGames(authData);
+        gameData = listOfAllGames;
         StringBuilder constructedListOFAllGames = new StringBuilder();
 
         for (int i = 0; i < listOfAllGames.length; i++) {
             constructedListOFAllGames.append((i+1));
-            constructedListOFAllGames.append(listOfAllGames[i].);
+            constructedListOFAllGames.append(") ");
+            constructedListOFAllGames.append(listOfAllGames[i].gameName());
+            constructedListOFAllGames.append(". White: ");
+            constructedListOFAllGames.append(listOfAllGames[i].whiteUsername());
+            constructedListOFAllGames.append(", Black: ");
+            constructedListOFAllGames.append(listOfAllGames[i].blackUsername());
+            constructedListOFAllGames.append("\n");
         }
+
+        return constructedListOFAllGames.toString();
     }
 
     public void logout() throws DataAccessException {
